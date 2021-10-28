70396c6009acf6df34963d41343547c908eb54a0
jiangzhiying <jiangzhiying@96966.com>
2021-10-28 17:21:48 +0800

    增加commander.option选项

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const commander = require("commander");
 const downgit = require("download-git-repo");
 const chalk = require("chalk");
 const ora = require("ora");
-const figlet = require("figlet")
+const figlet = require("figlet");
 
 commander.option("-a [num]", "this is test -a", (num) => {
   //[num]选填
@@ -12,20 +12,31 @@ commander.option("-a [num]", "this is test -a", (num) => {
   console.log("num :>> ", num);
   console.log("you input -a");
 });
-commander.command("init <type>").action((type) => {
-  var spinner = ora("下载中").start();
-  //第一个参数：github/gitlab:本体库作者名
-  //第二个参数：使用者当前cmd的地址 ，就是要下载到那个地址
-  //__dirname 当前项目地址
-  //process.cwd 当前所在的地址
-  //第三个参数：配置项 clone：true通过git clone 下载，默认为false，HTTP下载（会更快）
-  //第四个参数：回调函数
-  downgit("github:whonow0925/vite-vue3.0-template", process.cwd(), {}, (err) => {
-    if (err) throw err;
-    spinner.stop();
-    console.log(chalk.green("下载成功"));
+commander
+  .command("init <name>")
+  .description("init a new project")
+  // -f or --force 为强制创建，如果创建的目录存在则直接覆盖
+  .option("-f, --force", "overwrite target directory if it exist")
+  .action((name,options) => {
+    var spinner = ora("下载中").start();
+    //第一个参数：github/gitlab:本体库作者名
+    //第二个参数：使用者当前cmd的地址 ，就是要下载到那个地址
+    //__dirname 当前项目地址
+    //process.cwd 当前所在的地址
+    //第三个参数：配置项 clone：true通过git clone 下载，默认为false，HTTP下载（会更快）
+    //第四个参数：回调函数
+    downgit(
+      "github:whonow0925/vite-vue3.0-template",
+      process.cwd(),
+      {},
+      (err) => {
+        if (err) throw err;
+        spinner.stop();
+        console.log(chalk.green("下载成功"));
+      }
+    );
+    console.log("name:", name, "option", options);
   });
-});
 
 commander.on("--help", () => {
   console.log(
@@ -42,7 +53,10 @@ commander.on("--help", () => {
   console.log(`\r\nRun ${chalk.cyan(`roc <command> --help`)} show details\r\n`);
 });
 
-commander.version("1.0.0");
+commander
+  // .version(`v${require("./package.json").version}`)
+  .version("v1.0.0")
+  .usage("<command> [option]");
 
 commander.parse(process.argv);
 //process.argv 是node自带存放参数的一个数组
